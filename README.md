9fa6fac2d76f45916fd9d89edb68df00cb35caad
Munish Kumar <Munish.Kumar@hcl.com>
2020-10-16 14:09:56 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# wknd-muzik
+# WKND Muzik Site Project
+
+This is lean code using AEM Core Component:
+
+
+## How to build
+
+
+Run following command to deploy it to a Author instance
+
+    mvn clean install -PautoInstallPackage
+   
+Run following command to deploy it to a publish instance
+
+    mvn clean install -PautoInstallPackagePublish
+    
---
+++ ui.apps/src/main/content/jcr_root/apps/wknd-muzik/clientlibs/clientlib-base/js/base.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+
+    $("#back-to-top").click(function () {
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+
+    $("#wkend-search-icon .cmp-button__icon.cmp-button__icon--fa-search").click(function () {
+        $('#wkend-search').toggle('slow');
+    });
+
+    $("#wkend-language-icon .cmp-button__icon.cmp-button__icon--fa-globe").hover(function () {
+        $('#wkend-language-nav').show('slow');
+    });
+
+
+    $(".languagenavigation").mouseleave(function () {
+        $('#wkend-language-nav').hide('slow');
+    });
+
+    if(window.matchMedia("(max-width: 768px)").matches){
+        $("#wkend-top-nav").click(function () {
+            $('#wkend-top-nav').toggleClass('hamburger-nav');
+        });
+    }
+
+    $("#wkend-social a").attr('target', 'blank')
+
+	 $( ".wkend-teaser-music-list" ).each(function() {
+      	imageUrl = $(this).find('.cmp-teaser .cmp-teaser__image div').attr('data-asset');
+        $(this).find('.cmp-teaser').css("background-image", "url(" + imageUrl + ")");
+	});
+});
+
+$( window ).load(function() {
+
+
+
+});
